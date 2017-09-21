bc07b8e141c9d90581a39795ebf0ad8bdc04effc
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-21 23:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ an empty array stored in a variable. The function should also contain a for
 
 
 function theBeatlesPlay(musicians,instruments){
-  var derp = new Array()
+var derp = ['John Lennon plays guitar'];
 
   for (let i = 0; i == musicians.length-1;i++){
-    var msg = new String('John Lennon plays guitar');
+    var msg = new String('');
 
     /*if(i == 0){
 
