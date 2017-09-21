da5a435476ed2dc7ba98a78a7eeda67e119e8b50
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-21 23:39:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
 
 
-+ Create a function `theBeatlesPlay`, which accepts two parameters- an array of 
-musicians and an array of instruments. The body of the function should create 
++ Create a function `theBeatlesPlay`, which accepts two parameters- an array of
+musicians and an array of instruments. The body of the function should create
 an empty array stored in a variable. The function should also contain a for
- loop which loops over the array of musicians. You'll want to be careful 
+ loop which loops over the array of musicians. You'll want to be careful
  about what value you set your counter variable to store.
-  (Hint: Think about what the first index of an array is). 
+  (Hint: Think about what the first index of an array is).
   The first time through the loop, the body of the loop should create a string
-   using the first index of the musicians array and the first index of the instruments 
+   using the first index of the musicians array and the first index of the instruments
    array: `"John Lennon plays guitar"`. This string should be added to the empty array you created.
     The loop should make the same sentence for every member of the musicians array.
      The function should return the array of new strings.
@@ -20,5 +20,13 @@ an empty array stored in a variable. The function should also contain a for
 function theBeatlesPlay(musicians,instruments){
   var derp = Array(message)
 
-  
+for (let i = 0; musicians.length-1;i++){
+  if i == 0{
+    var msg = ""
+  }
+
+  msg = musicians[i] + " plays "+instruments[i]  
+  derp.push(msg)
+}
+  return derp  
 }
