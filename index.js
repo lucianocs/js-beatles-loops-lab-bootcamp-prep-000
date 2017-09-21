84bf355b0ad6f434734a1221a249c4abcb54255c
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-21 23:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function theBeatlesPlay(musicians,instruments){
   var derp = new Array()
 
   for (let i = 0; i == musicians.length-1;i++){
-    var msg = "John Lennon plays guitar"
+    var myString = new String('John Lennon plays guitar'); 
     /*if(i == 0){
 
     }else {
